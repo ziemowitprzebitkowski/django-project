b7537f566af352e2785669865e0cac3c62314459
Ziemowit Przebitkowski <ziemowit.przebitkowski@gmail.com>
2021-10-25 22:21:23 +0200

    [add-search] add sidebar styles and topic query handle

--- chat-project/base/views.py
+++ chat-project/base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q')
+    rooms = Room.objects.filter(topic__name=q)
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
