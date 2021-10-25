72e7585b012c3d6a3ccd3576669c46a9e47f0f11
Ziemowit Przebitkowski <ziemowit.przebitkowski@gmail.com>
2021-10-25 18:23:05 +0200

    [basic-configuration] add basic templates

--- chat-project/base/views.py
+++ chat-project/base/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 
 def room(request):
-    return HttpResponse('Room page')
+    return render(request, 'room.html')
